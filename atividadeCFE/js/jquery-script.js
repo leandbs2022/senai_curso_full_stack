70f9c95bec094e1006f52bc50646dc486ce6e54e
Leandro BSB <109764459+leandbs2022@users.noreply.github.com>
2023-11-15 13:34:10 -0300

    151120231334

--- atividadeCFE/js/jquery-script.js
+++ atividadeCFE/js/jquery-script.js
@@ -1,1 +1,18 @@
 //efeito de esconder formulário de cadastro
+
+/*
+Esse comando não funcionou e fiz de outra forma
+
+document.ready(function () {
+$('#botao-cadastrar').click(function() {
+        $('#form-cadastrar').slideToggle('slow')
+        $('#section-login').hide()
+        $('#botao-cadastrar').hide()
+    })
+})
+*/
+function hide(){
+$('#section-login').hide();
+$('#botao-cadastrar').hide();
+$('#form-cadastrar').slideToggle();
+}
